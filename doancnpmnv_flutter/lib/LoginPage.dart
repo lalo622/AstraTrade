b7c0a43825f3961302dbcb3f0ef200036d2ca2cc
hnahtIv <61571748+tuvithanh@users.noreply.github.com>
2025-11-18 12:18:26 +0700

    Reapply "Merge pull request #13 from lalo622/sync-api-to-android-project"
    This reverts commit 0a958ed5f452e6d8dc7004c51edd0aabeea484eb.

--- doancnpmnv_flutter/lib/LoginPage.dart
+++ doancnpmnv_flutter/lib/LoginPage.dart
@@ -101,7 +101,7 @@ class _LoginPageState extends State<LoginPage> {
             TextField(
               controller: _emailController,
               decoration: const InputDecoration(
-                labelText: "Email hoặc số điện thoại",
+                labelText: "Email",
                 border: OutlineInputBorder(),
               ),
             ),
